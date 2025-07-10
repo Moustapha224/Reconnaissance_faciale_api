362f8710f4e80e9215a4d970388636b35957f6a3
Moustapha224 <mssidiboss@gmail.com>
2025-07-10 20:18:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ shape_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 face_encoder = dlib.face_recognition_model_v1("dlib_face_recognition_resnet_model_v1.dat")
 
 # Fonction pour encoder les visages connus
-def load_known_faces(path="known_faces"):
+def load_known_faces(path="Known_faces"):
     encodings = []
     names = []
     for name in os.listdir(path):
