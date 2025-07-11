05377d0d9d51ee2e9fe26bdd39a4be8922559f65
Moustapha224 <mssidiboss@gmail.com>
2025-07-11 05:38:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -109,5 +109,9 @@ async def upload(file: UploadFile = File(...)):
         <a href="/">Revenir</a>
     """)
 
-##if __name__ == "__main__":
-    ##uvicorn.run(app, host="0.0.0.0", port=8000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
